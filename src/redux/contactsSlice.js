e1cd81fb4638df2a3d0a183efacec7c8fab2b379
Volodymyr Vovnenko <vovnenko.volodymyr@gmail.com>
2024-09-01 00:10:57 +0300

    refactor with redux-persist

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -4,28 +4,7 @@ import { nanoid } from "nanoid";
 const slice = createSlice({
   name: "contacts",
   initialState: {
-    items: [
-      {
-        id: nanoid(),
-        name: "Rosie Simpson",
-        number: "459-12-56",
-      },
-      {
-        id: nanoid(),
-        name: "Hermione Kline",
-        number: "443-89-12",
-      },
-      {
-        id: nanoid(),
-        name: "Eden Clements",
-        number: "645-17-79",
-      },
-      {
-        id: nanoid(),
-        name: "Annie Copeland",
-        number: "227-91-26",
-      },
-    ],
+    items: [],
   },
   reducers: {
     addContact: {
